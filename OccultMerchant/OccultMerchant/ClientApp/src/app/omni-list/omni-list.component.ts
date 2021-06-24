cee88940309f4ed6dd30b1df31720db23df1d792
mrfizban <fizban94@gmail.com>
2021-06-24 15:32:45 +0200

    base

---
+++ OccultMerchant/OccultMerchant/ClientApp/src/app/omni-list/omni-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-omni-list',
+  templateUrl: './omni-list.component.html',
+  styleUrls: ['./omni-list.component.css']
+})
+export class OmniListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
