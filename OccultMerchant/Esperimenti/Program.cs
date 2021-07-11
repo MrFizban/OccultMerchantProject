e43b25815f696d0a03aaaa1ee0c6962e0f480206
PopOsMrFizban <fizban94@gmail.com>
2021-07-11 17:31:51 +0200

    EE funziona impementare find

---
+++ OccultMerchant/Esperimenti/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Esperimenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid a = Guid.Parse("93aa2119-b5af-48ed-97c0-a61c3f52d717");
+            Guid b = Guid.Parse("93AA2119-B5AF-48ED-97C0-A61C3F52D717");
+            Console.WriteLine(a.CompareTo(b) == 0);
+            
+        }
+    }
+}
---
+++ OccultMerchant/SqlObjectReadre/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SqlObjectReadre
+{
+    public class database
+    {
+        
+    }
+}
